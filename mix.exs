16ac4caefde7f4dd80cb32c733deb6fe10f883cf
Lukasz Badura <lukasz@getbase.com>
2017-01-12 10:25:38 +0100

    Bump to Elixir 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jito.Mixfile do
   def project do
     [app: :jito,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -18,8 +18,8 @@ defmodule Jito.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Jito, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :mariaex, :jira, :todoist]]
+      extra_applications: [:logger],
+    ]
   end
 
   # Specifies which paths to compile per environment.
